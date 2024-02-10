f1b96753fb3bb6e204c10657a8327dd128a1423b
Hayk <haykqaramyan09@gmail.com>
2024-02-10 08:25:46 -0800

    aab

--- 1.py
+++ 1.py
@@ -1,13 +1,27 @@
-import pygame
+import pygame, sys
 
+a = int(input("Enter a number: "))
+b = int(input("Enter a second number: "))
+c = a + b
 pygame.init()
-
+clock = pygame.time.Clock()
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 800
+BG_COLOR = (255, 255, 255)
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+screen.fill(BG_COLOR)
+img1= pygame.image.load("./python_photo.png").convert()
+screen.blit(img1, (0, 0))
+font = pygame.font.SysFont('freesansbold.ttf', 24)
+d = font.render(f"{c}", True, 'red')
+screen.blit(d, (20, 20))
 
-pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
+    pygame.display.update()
+    pygame.display.flip()
+    clock.tick(60)
